4f456151b93caefeebb15f01bdfd3b0238767fd3
roomanidzee <steampart@gmail.com>
2021-10-03 12:50:52 +0300

    formatting

--- python/apps/kafka_producer.py
+++ python/apps/kafka_producer.py
@@ -10,18 +10,23 @@ from domain import PersonRecord, read_data_from_csv
 
 # limit just for testing purposes
 async def send(loop, records: List[PersonRecord], limit=1000000):
-    producer = AIOKafkaProducer(loop=loop, bootstrap_servers="localhost:9093",)
+    producer = AIOKafkaProducer(
+        loop=loop,
+        bootstrap_servers="localhost:9093",
+    )
     await producer.start()
 
     counter = 0
 
     for record in records:
 
-        await producer.send_and_wait("persons-json", json.dumps(record.to_json()).encode())
+        await producer.send_and_wait(
+            "persons-json", json.dumps(record.to_json()).encode()
+        )
         await producer.send_and_wait("persons-avro", record.serialize())
         counter += 1
 
-        print(f'sent record {record}')
+        print(f"sent record {record}")
 
         if counter == limit:
             break
@@ -31,6 +36,7 @@ async def send(loop, records: List[PersonRecord], limit=1000000):
         print("Stoping producer...")
 
     await producer.stop()
+
 
 if __name__ == "__main__":
     data_path = Path(__file__).resolve().parents[2] / Path("resources") / Path("data")
